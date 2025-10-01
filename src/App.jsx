5c988a6d0ddcbcf342ee8dbc10b9374a416e9515
ZachariahRedfield <zjredfield@student.fullsail.edu>
2025-10-01 16:27:11 -0400

    Added React Imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import React from "react";
 import MapCanvas from "./components/MapCanvas";
 
 function App() {
+    console.log("App loaded");
+
   return (
     <div style={{ textAlign: "center", padding: "20px", background: "#111", color: "white" }}>
       <h1>Nat-1 Game</h1>
+      <p>React is running!</p>
       <MapCanvas />
     </div>
   );
